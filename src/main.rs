e2f83c540df65fa2d2ea3aece5295cb9ae213dfa
Fairbrook <kevinvr@hotmail.es>
2022-09-19 11:26:16 -0500

    Cleaning

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use crate::neural::{
     network::Network,
 };
 
-pub mod neural;
 pub mod file;
+pub mod neural;
 
 fn main() {
     let reader = file::reader::main("XOR_trn.csv");
@@ -22,35 +22,25 @@ fn main() {
         learning_rate: 0.76,
     };
 
-    /*
-        let inputs = vec![
-        vec![0.0, 0.0],
-        vec![0.0, 1.0],
-        vec![1.0, 0.0],
-        vec![1.0, 1.0],
-    ]; */
-
-    //let expected = vec![vec![-1.0], vec![1.0], vec![1.0], vec![1.0]];
-
     let mut error_avg = 1.0;
-    while error_avg > 0.001 {
+    let mut gen = 1;
+    while error_avg > 0.0001 {
         error_avg = 0.0;
+        println!("----- Entrenando generación {} -----", gen);
         for (i, input) in reader.inputs.iter().enumerate() {
             let vector_input = DVector::from_vec(input.clone());
             let vector_expected = DVector::from_vec(reader.expected[i].clone());
             let output = network.full_forward(&vector_input);
             let error = SquaredError::calc(&output, &vector_expected)[0];
             error_avg += error;
-            println!(
-                "got {} expected {}, error {}",
-                output[0], vector_expected[0], error
-            );
             network.epoch::<SquaredError>(&vector_input, &vector_expected);
         }
+        gen += 1;
         error_avg = error_avg / reader.inputs.len() as f32;
+        println!("\tError promedio: {}", error_avg);
     }
 
-    let vector_input = DVector::from_vec(vec![0.0, 0.0]);
+    let vector_input = DVector::from_vec(vec![-1.0, -1.0]);
     let vector_expected = DVector::from_vec(vec![-1.0]);
     let output = network.full_forward(&vector_input);
     println!("got {} expected {}", output[0], vector_expected[0]);
